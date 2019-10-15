fc6471987a77d02059f267b048901226c38efcc3
miladiashe <55428028+miladiashe@users.noreply.github.com>
2019-10-15 15:35:54 +0900

    4qjs

--- main.c
+++ main.c
@@ -3,37 +3,18 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int sumtwo( int a, int b)
+int square(int n);
+
+
+int main(void) 
 {
-	int output;
-	output = a + b;
-	return output;
+	int result;
+	result = square(5);
+	printf("%d", result);
+
 }
 
-int square( int n )
+int square(int n)
 {
-	return (n*n);
-}
-
-int get_max( int a, int b )
-{
-	if( a > b ) return(a);
-	else return(b);
-}
-
-int main(int argc, char *argv[]) 
-{
-	int a, b;
-	int output1;
-	a = 2;
-	b = 5;	
-
-	output1 = sumtwo(a,b);
-	printf("sumtwo = %d\n", output1);
-	
-	printf("square = %d\n", square(b));
-	
-	printf("%d is bigger\n", get_max(a,b));
-	
-	return 0;
+	return(n*n);
 }
