482d2c23e0650ab386e8ce33bdf55f612fa001ac
miladiashe <55428028+miladiashe@users.noreply.github.com>
2019-10-15 16:09:31 +0900

    7qjs!

--- main.c
+++ main.c
@@ -4,15 +4,44 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+int factorial(int a)
+{ 
+	int res = 1;
+	int i;
+	for (i=1; i!=a; i++)
+		res = res*i;
+	return res;
+}
+
+
+
+int calcombination(int n, int r)
+{
+	int u, d;
+	u = factorial(n);
+	d = factorial(n-r)*factorial(r);
+	
+	return(u/d);
+}
+
 
 int main(int argc, char *argv[]) 
 {
-	int i;
-	int max = 45;
+	int c, n, r;
 	
-	srand( (unsigned) time(NULL));
-	for (i=0; i<6; i++)
-		printf("%d", 1+rand()%max);
+	printf("C(n,r) function \n");
+	printf("insert n = \n");
+	scanf("%d", &n);
+	printf("insert r = \n");
+	scanf("%d", &r);
+	
+	c = calcombination(n, r);
+	
+	printf("%d %d\n", n,r);
+	printf("C= %d", c);
+	
+	
+	
 
 return 0;
 
